39df26155aa53b09f3a8d344f29f24c46f6a8078
mohan-vlsi <mohanmeesala04@gmail.com>
2026-07-10 13:19:28 +0530

    Update cpu_pkg.sv

--- rtl/core/cpu_pkg.sv
+++ rtl/core/cpu_pkg.sv
@@ -1,6 +1,16 @@
-typedef enum logic [3:0] {
+package cpu_pkg;
+
+typedef enum logic [3:0]
+{
     ALU_ADD,
     ALU_SUB,
     ALU_AND,
-    ...
+    ALU_OR,
+    ALU_XOR,
+    ALU_SLL,
+    ALU_SRL,
+    ALU_SRA,
+    ALU_SLT
 } alu_op_t;
+
+endpackage
